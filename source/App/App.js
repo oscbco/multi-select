7de85517f08c531fca3fc502830c277795e00098
Oscar Barrera <oscbco@gmail.com>
2019-07-26 16:40:10 -0600

    Upgraded client code to use the latest props

--- source/App/App.js
+++ source/App/App.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 
 import MultiSelect from '../MultiSelect/MultiSelect';
 import './_App.scss';
@@ -13,33 +13,32 @@ import cssOrangeWithDashes from './sass/orangeWithDashes.scss';
 
 import { programmingLanguages } from './data';
 
-export default class App extends PureComponent {
-  render () {
-    return (
-      <React.Fragment>
-        <h1>MultiSelect</h1>
-        <h2>Styling Examples</h2>
-        <h3>Monochrome</h3>
-        <MultiSelect items={programmingLanguages} width='400px' classes={cssBasic} />
+export default function App () {
+  const [tags, setTags] = useState([]);
+  return (
+    <React.Fragment>
+      <h1>MultiSelect</h1>
+      <h2>Styling Examples</h2>
+      <h3>Monochrome</h3>
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssBasic} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
-        <h3>Orange</h3>
-        <MultiSelect items={programmingLanguages} width='400px' classes={cssOrange} />
+      <h3>Orange</h3>
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssOrange} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
-        <h3>Orange gradient with separators</h3>
-        <MultiSelect items={programmingLanguages} width='400px' classes={cssOrangeWithSeparators} />
+      <h3>Orange gradient with separators</h3>
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssOrangeWithSeparators} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
-        <h3>Orange with dashes</h3>
-        <MultiSelect items={programmingLanguages} width='400px' classes={cssOrangeWithDashes} />
+      <h3>Orange with dashes</h3>
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssOrangeWithDashes} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
-        <h3>Dark</h3>
-        <MultiSelect items={programmingLanguages} width='400px' classes={cssDark} />
+      <h3>Dark</h3>
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssDark} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
-        <h3>Blue</h3>
-        <MultiSelect items={programmingLanguages} width='400px' classes={cssBlue} />
+      <h3>Blue</h3>
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssBlue} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
-        <h3>Style: background: green</h3>
-        <MultiSelect items={programmingLanguages} width='400px' classes={cssGreen} />
-      </React.Fragment>
-    );
-  }
+      <h3>Style: background: green</h3>
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssGreen} setSelected={setTags} selected={tags} placeholder='Select tags' />
+    </React.Fragment>
+  );
 }
