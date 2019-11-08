3a7e61e2a06ca58bab626fccc45a7a8595227b83
Oscar Barrera <oscbco@gmail.com>
2019-11-08 16:31:19 -0600

    Added a new example showcasing use with a regular textbox to the side

--- source/App/App.js
+++ source/App/App.js
@@ -16,17 +16,29 @@ import { programmingLanguages } from './data';
 export default function App () {
   const [tags, setTags] = useState([]);
   return (
-    <React.Fragment>
+    <>
       <h1>MultiSelect</h1>
       <h2>Styling Examples</h2>
+
+      <h3>Style: background: Monochrome</h3>
+      <div className={cssBasic.wrapper}>
+        <span className={cssBasic.lblBasic}>
+          <span>
+            Text Label:&nbsp;
+          </span>
+        </span>
+        <input type='text' defaultValue='a' className={cssBasic.txtControl} />
+        <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssBasic} setSelected={setTags} selected={tags} placeholder='Select tags' />
+      </div>
+
       <h3>Monochrome</h3>
-      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssBasic} setSelected={setTags} selected={tags} placeholder='Select tags' />
+      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssGreen} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
       <h3>Orange</h3>
       <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssOrange} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
       <h3>Orange gradient with separators</h3>
-      <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssOrangeWithSeparators} setSelected={setTags} selected={tags} placeholder='Select tags' />
+      Orange gradient with separators: <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssOrangeWithSeparators} setSelected={setTags} selected={tags} placeholder='Select tags' />
 
       <h3>Orange with dashes</h3>
       <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssOrangeWithDashes} setSelected={setTags} selected={tags} placeholder='Select tags' />
@@ -39,6 +51,7 @@ export default function App () {
 
       <h3>Style: background: green</h3>
       <MultiSelect id='id' label='label' items={programmingLanguages} width='400px' classes={cssGreen} setSelected={setTags} selected={tags} placeholder='Select tags' />
-    </React.Fragment>
+
+    </>
   );
 }
