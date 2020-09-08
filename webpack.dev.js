d1fbf9132e14e68cad8044d8dd1cb6b699b7ebbe
Oscar Barrera <oscbco@gmail.com>
2020-09-07 21:47:53 -0600

    Reconfigure css-loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -52,8 +52,9 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
-              localsConvention: 'camelCase',
+              modules: {
+                exportLocalsConvention: 'camelCase'
+              },
               sourceMap: true
             }
           },
